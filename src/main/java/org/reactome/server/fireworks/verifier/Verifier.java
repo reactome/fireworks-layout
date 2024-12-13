2306140a69b6a0f7b34d9ca0342580d134d33635
Joel Weiser <jweiser13@protonmail.com>
2024-12-12 22:02:42 -0500

    adds comparision of fireworks file sizes to previous release

--- src/main/java/org/reactome/server/fireworks/verifier/Verifier.java
+++ src/main/java/org/reactome/server/fireworks/verifier/Verifier.java
@@ -3,6 +3,7 @@ package org.reactome.server.fireworks.verifier;
 import com.martiansoftware.jsap.*;
 import org.reactome.server.fireworks.Main;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,6 +11,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.reactome.release.verifier.CountUtils.greaterThanOrEqualTo5PercentDrop;
+import static org.reactome.release.verifier.FileUtils.downloadFileFromS3;
+import static org.reactome.release.verifier.FileUtils.untarTgzFile;
 
 /**
  * @author Joel Weiser (joel.weiser@oicr.on.ca)
@@ -19,7 +24,7 @@ public class Verifier {
     private String outputDirectory;
     private int releaseNumber;
 
-    public static void main(String[] args) throws JSAPException {
+    public static void main(String[] args) throws JSAPException, IOException {
         Verifier verifier = new Verifier();
         verifier.parseCommandLineArgs(args);
         verifier.run();
@@ -41,7 +46,7 @@ public class Verifier {
         this.releaseNumber = config.getInt("releaseNumber");
     }
 
-    public void run() {
+    public void run() throws IOException {
         List<String> errorMessages = verifyFireworksLayoutRanCorrectly();
         if (errorMessages.isEmpty()) {
             System.out.println("Fireworks Layout has run correctly!");
@@ -51,7 +56,7 @@ public class Verifier {
         }
     }
 
-    private List<String> verifyFireworksLayoutRanCorrectly() {
+    private List<String> verifyFireworksLayoutRanCorrectly() throws IOException {
         List<String> errorMessages = new ArrayList<>();
 
         errorMessages.addAll(checkFireworksFolderExists());
@@ -70,9 +75,7 @@ public class Verifier {
     private List<String> checkJSONFilesForAllSpeciesExist() {
         List<String> errorMessages = new ArrayList<>();
 
-        List<String> jsonFileNames = getJSONFileNamesForAllSpecies();
-        for (String jsonFileName : jsonFileNames) {
-            Path jsonFilePath = Paths.get(this.outputDirectory, jsonFileName);
+        for (Path jsonFilePath : getJSONFilePathsForAllSpecies()) {
             if (!Files.exists(jsonFilePath)) {
                 errorMessages.add("File " + jsonFilePath + " does not exist");
             }
@@ -81,8 +84,45 @@ public class Verifier {
         return errorMessages;
     }
 
-    private List<String> checkJSONFileSizesComparedToPreviousRelease() {
-        return new ArrayList<>();
+    private List<String> checkJSONFileSizesComparedToPreviousRelease() throws IOException {
+        List<String> errorMessages = new ArrayList<>();
+
+        removeDownloadedFileIfExists(getPreviousReleaseFireworksFileName());
+        downloadFileFromS3("reactome", getPreviousReleaseFireworksFilePathInS3());
+        untarTgzFile(getPreviousReleaseFireworksFileName(),".");
+
+        for (Path jsonFilePath : getJSONFilePathsForAllSpecies()) {
+            if (Files.exists(jsonFilePath)) {
+                if (!Files.exists(getPreviousJSONFilePath(jsonFilePath))) {
+                    errorMessages.add("Previous version JSON file " + getPreviousJSONFilePath(jsonFilePath) +
+                        " does not exist for comparison");
+                    continue;
+                }
+
+                long actualSizeInBytes = Files.size(jsonFilePath);
+                long expectedSizeInBytes = Files.size(getPreviousJSONFilePath(jsonFilePath));
+
+                if (greaterThanOrEqualTo5PercentDrop(actualSizeInBytes, expectedSizeInBytes)) {
+                    errorMessages.add(jsonFilePath + " has too small size " +
+                        "(actual: " + actualSizeInBytes + " bytes) " +
+                        "(expected: " + expectedSizeInBytes + " bytes) - " +
+                        "difference of " + (expectedSizeInBytes - actualSizeInBytes) + " bytes");
+                }
+            }
+        }
+
+        return errorMessages;
+    }
+
+    private void removeDownloadedFileIfExists(String downloadedFile) throws IOException {
+        Files.deleteIfExists(Paths.get(downloadedFile));
+    }
+
+    private List<Path> getJSONFilePathsForAllSpecies() {
+        return getJSONFileNamesForAllSpecies()
+            .stream()
+            .map(jsonFileName -> Paths.get(this.outputDirectory, jsonFileName))
+            .collect(Collectors.toList());
     }
 
     private List<String> getJSONFileNamesForAllSpecies() {
@@ -92,5 +132,22 @@ public class Verifier {
             "Schizosaccharomyces_pombe", "Sus_scrofa", "Xenopus_tropicalis");
 
         return species.stream().map(sp -> sp.concat(".json")).collect(Collectors.toList());
+    }
+
+    private String getPreviousReleaseFireworksFilePathInS3() {
+        return "private/releases/" + getPreviousReleaseVersion() + "/fireworks/data/" +
+            getPreviousReleaseFireworksFileName();
+    }
+
+    private String getPreviousReleaseFireworksFileName() {
+        return "fireworks-v" + getPreviousReleaseVersion() + ".tgz";
+    }
+
+    private Path getPreviousJSONFilePath(Path currentJsonFilePath) {
+        return Paths.get("fireworks", currentJsonFilePath.getFileName().toString());
+    }
+
+    private int getPreviousReleaseVersion() {
+        return this.releaseNumber - 1;
     }
 }
